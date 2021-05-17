aa0d43b0d651edfd183bcaeb7495a89ac68f4547
te26Oku <teturou@aw.wakwak.com>
2021-05-17 22:14:17 +0900

    クリックした座標にマーカーを配置する処理を作成

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ var map = L.map('map', {
     center: [35.66572, 139.73100],
     zoom: 17,
   });
-  var tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-  });
-  tileLayer.addTo(map);
+var tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+attribution: '© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+});
+tileLayer.addTo(map);
+
+map.on('click', function(e) {
+    lat = e.latlng.lat;
+    lng = e.latlng.lng;
+    console.log("lat: " + lat + ", lng: " + lng);
+    L.marker([lat, lng]).addTo(map); 
+} );
