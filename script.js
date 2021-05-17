dfa3139a180a2a88601e3e7435353871068163c5
te26Oku <teturou@aw.wakwak.com>
2021-05-17 22:41:47 +0900

    現在地を監視する処理を追加

--- script.js
+++ script.js
@@ -18,8 +18,7 @@ map.on('click', function(e) {
     var p = [lat, lng];
     positions.push(p);
     L.marker([lat, lng]).addTo(map); 
-} );
-
+});
 
 function getDistance(pos1, pos2) {
     var [lat1, lng1] = pos1;
@@ -29,4 +28,33 @@ function getDistance(pos1, pos2) {
     lat2 *= Math.PI / 180;
     lng2 *= Math.PI / 180;
     return 6371 * Math.acos(Math.cos(lat1) * Math.cos(lat2) * Math.cos(lng2 - lng1) + Math.sin(lat1) * Math.sin(lat2));
+}
+
+
+
+var id, target, options;
+
+function success(pos) {
+  var crd = pos.coords;
+  if (target.latitude === crd.latitude && target.longitude === crd.longitude) {
+    console.log('Congratulations, you reached the target');
+    navigator.geolocation.clearWatch(id);
   }
+}
+
+function error(err) {
+  console.warn('ERROR(' + err.code + '): ' + err.message);
+}
+
+target = {
+  latitude : 0,
+  longitude: 0
+};
+
+options = {
+  enableHighAccuracy: false,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+id = navigator.geolocation.watchPosition(success, error, options);
